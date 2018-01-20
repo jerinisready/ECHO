f0bce4f3ba068eddb387512658973b707a500248
sidharth <sidhu.svs@gmail.com>
2018-01-20 13:02:18 +0530

    new data

--- payment/admin.py
+++ payment/admin.py
@@ -4,3 +4,7 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
+#from website.models import account , topic
+
+#admin.site.register(account)
+#admin.site.register(topic)
--- website/views.py
+++ website/views.py
@@ -2,5 +2,14 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.template import loader
 # Create your views here.
+
+def home(request):
+    template = loader.get_template('website/index.html')
+    context = {
+
+    }
+    return HttpResponse(template.render(context, request))
+    # instead we can use return render(request, 'website/index.html', context)
